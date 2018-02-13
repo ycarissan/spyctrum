4233f003a7ef1eab437b73a3b6191697f74f77de
ycarissan <yannick.carissan@univ-amu.fr>
2018-02-13 10:08:38 +0100

    we can scna over gamma and shift now! Celebrate!

--- plot_spectra.py
+++ plot_spectra.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
 import argparse
+import datetime
 from spectrum import *
+from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt 
 import logging
 import numpy as np
@@ -15,7 +17,8 @@ def main():
    parser.add_argument("-c", "--cd", help="CD data file", default="refcd.csv")
    parser.add_argument("-s", "--shift", help="shift value on the energies", type=float, default=1.0)
    parser.add_argument("-g", "--gamma", help="gamma value in eV", type=float, default=0.25)
-   parser.add_argument("-r", "--gamma_range", help="gamma min max step values in eV", type=float, nargs=3, default=None)
+   parser.add_argument("-r", "--gamma_range", help="gamma min max nstep values in eV", type=float, nargs=3, default=None)
+   parser.add_argument("-v", "--shift_range", help="shift min max nstep values in eV", type=float, nargs=3, default=None)
    args = parser.parse_args()
 #Default values
    phase=1
@@ -24,12 +27,19 @@ def main():
    shift=args.shift
    gamma=args.gamma
    gammaRange=args.gamma_range
+   shiftRange=args.shift_range
    if args.phase:
       print "Phase argument toggled"
       phase=-1
    if gammaRange!=None:
-      gammaRange=np.linspace(args.gamma_range[0],args.gamma_range[1],args.gamma_range[2])
+      gammaRange=np.linspace(args.gamma_range[0], args.gamma_range[1], args.gamma_range[2])
       MODE="scanGamma"
+   if shiftRange!=None:
+      shiftRange=np.linspace(args.shift_range[0], args.shift_range[1], args.shift_range[2])
+      if MODE=="scanGamma":
+         MODE="scanGamma scanShift"
+      else:
+         MODE="scanShift"
    logging.info('SPYCTRUM a program better than its name')
    escfout = args.output
    refuvcsv = args.uv
@@ -63,10 +73,15 @@ def main():
    logging.info( "  found {0} wavelength".format(len(wl)))
    if MODE=="single":
       spectrumTh = Spectrum.SpectrumThfactory(wl, uv, cd, phase, 200, 450, gamma, shift)
-   elif MODE=="scanGamma":
+   elif MODE.find("scanGamma")>=0:
       spectrumTh = []
       for gamma in gammaRange:
-         spectrumTh.append(SpectrumThfactory(wl, uv, cd, phase, 200, 450, gamma, shift))
+         if MODE.find("scanShift")>=0:
+            for shift in shiftRange:
+               spectrumTh.append(Spectrum.SpectrumThfactory(wl, uv, cd, phase, 200, 450, gamma, shift))
+         else:
+            spectrumTh.append(Spectrum.SpectrumThfactory(wl, uv, cd, phase, 200, 450, gamma, shift))
+   logging.info( "  {0} spectra generated".format(len(spectrumTh)))
 #
    if MODE=="single":
       uv_th=spectrumTh.getUV()
@@ -89,9 +104,44 @@ def main():
       lbl=[l.get_label() for l in leg]
       axCD2.legend(leg, lbl, loc="upper right")
       axCD.grid(True, which="both")
-   axCD.axhline(y=0, color='k')
+      axCD.axhline(y=0, color='k')
 #
-   plt.show()
+      plt.show()
+   elif MODE.find("scanGamma")>=0 or  MODE.find("scanShift")>=0:
+      with PdfPages('spyctrum_pdf.pdf') as pdf:
+         for sp in spectrumTh:
+            uv_th=sp.getUV()
+            cd_th=sp.getCD()
+            logging.info( "Plotting")
+            plt.title('Page xXx')
+            fig, (axUV, axCD) = plt.subplots(ncols=1,nrows=2)
+#UV bloc
+            legUV = axUV.plot(x, uv_exp, '-', label="UV Exp")
+            axUV2 = axUV.twinx()
+            legUV2 = axUV2.plot(x, uv_th, '--', label="UV Th")
+            leg=legUV+legUV2
+            lbl=[l.get_label() for l in leg]
+            axUV.legend(leg, lbl, loc="upper right")
+            axUV.grid(True, which="both")
+#CD bloc
+            legCD = axCD.plot(x, cd_exp, '-' , label="CD Exp")
+            axCD2 = axCD.twinx()
+            legCD2 = axCD2.plot(x, cd_th, '--', label="CD Th")
+            leg=legCD+legCD2
+            lbl=[l.get_label() for l in leg]
+            axCD2.legend(leg, lbl, loc="upper right")
+            axCD.grid(True, which="both")
+            axCD.axhline(y=0, color='k')
+            pdf.savefig(fig)  # saves the current figure into a pdf page
+            plt.close()
+         # We can also set the file's metadata via the PdfPages object:
+         d = pdf.infodict()
+         d['Title'] = 'Multipage PDF scan over gamma values'
+         d['Author'] = 'auto'
+         d['Subject'] = 'Scan over gamma values'
+         d['Keywords'] = 'scan gamma UV CD'
+         d['CreationDate'] = datetime.datetime(2018, 02, 13)
+         d['ModDate'] = datetime.datetime.today()
 
 if __name__ == '__main__':
     main()
